e4afc4aae3e3f0666961f4203fcbf3dfa897ceea
Joy <thegoatjoy@yahoo.com>
2025-01-19 19:49:52 +0600

    repearing

--- 2nd_for_git.py
+++ 2nd_for_git.py
@@ -3,9 +3,9 @@ import random
 turtle.shape("turtle")
 
 turtle.speed(0)
-for i in range(100):
-    x = random.randint(-200,200)
-    y = random.randint(-200,200)
+for i in range(200):
+    x = random.randint(-300,300)
+    y = random.randint(-250,250)
     turtle.setposition(x,y)
     turtle.dot()
 turtle.done()
